15fb5ecc6161af668d3e337bf47a2c09adf41a33
FSGabrsek <f.gabrsek@gmail.com>
2024-07-09 11:45:35 +0200

    added comments

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ client_secret = getenv("CLIENT_SECRET")
 app_principal_id = getenv("APP_PRINCIPAL_ID")
 role_id = getenv("ROLE_ID")
 
+# request a token for an application client with User.ReadWrite.All permissions
 def client_credentials() -> str:
     r = requests.post(
         "https://login.microsoftonline.com/myhmb2c.onmicrosoft.com/oauth2/v2.0/token",
@@ -23,6 +24,7 @@ def client_credentials() -> str:
 
     return r.json()["access_token"]
 
+# POST a new appRoleAssigment for the app resource principal on the user
 def grant_assignment(user_id: str) -> dict:
     r = requests.post(
         f"https://graph.microsoft.com/v1.0/users/{user_id}/appRoleAssignments",
@@ -38,6 +40,7 @@ def grant_assignment(user_id: str) -> dict:
     
     return r.json()
 
+# GET appRoleAssignments filtered for the app resource and return the first assignment
 def get_assignment(user_id: str) -> dict:
     r = requests.get(
         f"https://graph.microsoft.com/v1.0/users/{user_id}/appRoleAssignments?$filter=resourceId eq {app_principal_id}",
@@ -45,6 +48,7 @@ def get_assignment(user_id: str) -> dict:
     
     return r.json()
 
+# DELETE the appRoleAssignment for the user on the app resource principal
 def revoke_assignment(user_id: str) -> bool:
     r = requests.delete(
         f"https://graph.microsoft.com/v1.0/users/{user_id}/appRoleAssignments/{get_assignment(user_id)["value"][0]["id"]}",
